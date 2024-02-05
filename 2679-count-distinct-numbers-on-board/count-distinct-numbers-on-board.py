96c86b94f397e4313930fe54496adb03cba7f88f
Krishna-mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-05 12:04:19 -0500

    Time: 34 ms (77.10%) | Memory: 16.4 MB (79.61%) - LeetSync

--- 2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.py
+++ 2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.py
@@ -1,9 +1,4 @@
 class Solution:
     def distinctIntegers(self, n: int) -> int:
-        res={n}
-        for i in range(n,1,-1):
-            for j in range(2,i):
-                if i%j == 1:
-                    res.add(j)
-        return len(res)
+        return n-1 if n>1 else 1
         
