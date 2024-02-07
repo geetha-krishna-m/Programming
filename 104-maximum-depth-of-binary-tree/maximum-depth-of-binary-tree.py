b8c9e6bfba3cf55972a9c989a0141917e31bec54
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-07 01:29:21 -0500

    Time: 41 ms (63.10%) | Memory: 17.6 MB (94.71%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -4,16 +4,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         maxy = 0
+#         def traverse(root,curr_max):
+#             nonlocal maxy
+#             if root == None:
+#                 maxy = curr_max if curr_max > maxy else maxy
+#                 curr_max -= 1
+#                 return maxy
+#             left = traverse(root.left,curr_max + 1)
+#             right = traverse(root.right,curr_max + 1)
+#             return right
+#         return traverse(root,0)
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         maxy = 0
         def traverse(root,curr_max):
             nonlocal maxy
             if root == None:
-                maxy = curr_max if curr_max > maxy else maxy
-                curr_max -= 1
-                return maxy
-            left = traverse(root.left,curr_max + 1)
-            right = traverse(root.right,curr_max + 1)
-            return right
-        return traverse(root,0)
+                return
+            traverse(root.left,curr_max+1)
+            traverse(root.right,curr_max+1)
+            maxy = max(maxy,curr_max)
+        traverse(root,1)
+        return maxy
