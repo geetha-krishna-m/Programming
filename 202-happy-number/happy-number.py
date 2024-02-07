9adc8231301d2dd632007495240b323cecf04ce8
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-07 07:43:21 -0500

    Time: 41 ms (45.17%) | Memory: 16.7 MB (51.98%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,21 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def find(num):
-            squares = 0
+        def find(num,squares):
             while(num!=0):
                 squares += (num%10)**2
                 num = num // 10
             return squares
-        visited = set()
-        while True:
-            if n==1:
-                return True
-            if n in visited:
-                return False
-            visited.add(n)
-            n = find(n)
-        return False
-
-        
-
-        
+        if(n==1):
+            return True
+        slow,fast = find(n,0),find(find(n,0),0)
+        if slow == 1:
+            return True
+        while(slow!=fast):
+            slow = find(slow,0)
+            fast = find(find(fast,0),0)
+        return slow==1
