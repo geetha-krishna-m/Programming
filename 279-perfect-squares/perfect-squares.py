deeb6629f85b10830f6bd16b47170642a0db79b4
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-08 19:13:06 -0500

    Time: 2695 ms (43.79%) | Memory: 16.8 MB (75.00%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -1,17 +1,29 @@
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+#         cache = [-1]*(n+1)
+#         def solve(n):
+#             if n == 0:
+#                 return 0
+#             if cache[n]!=-1:
+#                 return cache[n]
+#             mini,i = n,1
+#             while i*i<=n:
+#                 pSquare = i*i
+#                 ans = 1 + solve(n-pSquare)
+#                 mini = min(ans,mini)
+#                 i += 1
+#             cache[n] = mini
+#             return mini
+#         return solve(n)
 class Solution:
     def numSquares(self, n: int) -> int:
-        cache = [-1]*(n+1)
-        def solve(n):
-            if n == 0:
-                return 0
-            if cache[n]!=-1:
-                return cache[n]
-            mini,i = n,1
-            while i*i<=n:
-                pSquare = i*i
-                ans = 1 + solve(n-pSquare)
-                mini = min(ans,mini)
-                i += 1
-            cache[n] = mini
-            return mini
-        return solve(n)
+        memo = [float('inf')] * (n + 1)
+        memo[0] = 0
+        
+        for i in range(1, n + 1):
+            j = 1
+            while j * j <= i:
+                memo[i] = min(memo[i], 1 + memo[i - j * j])
+                j += 1
+                
+        return memo[n]
