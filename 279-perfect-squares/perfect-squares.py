62f555459edb9db19831866d9a0756d217fe74b7
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-08 19:30:57 -0500

    Time: 35 ms (99.48%) | Memory: 16.6 MB (89.48%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -15,15 +15,36 @@
 #             cache[n] = mini
 #             return mini
 #         return solve(n)
-class Solution:
-    def numSquares(self, n: int) -> int:
-        memo = [float('inf')] * (n + 1)
-        memo[0] = 0
+# class Solution:
+#     def numSquares(self, n: int) -> int:
+#         memo = [float('inf')] * (n + 1)
+#         memo[0] = 0
         
-        for i in range(1, n + 1):
-            j = 1
-            while j * j <= i:
-                memo[i] = min(memo[i], 1 + memo[i - j * j])
-                j += 1
+#         for i in range(1, n + 1):
+#             j = 1
+#             while j * j <= i:
+#                 memo[i] = min(memo[i], 1 + memo[i - j * j])
+#                 j += 1
                 
-        return memo[n]
+#         return memo[n]
+class Solution:
+    
+    def numSquares(self, n: int) -> int:
+        #using lagranges four-square theorem 
+        def isSquare(n):
+            if(math.ceil(n**0.5) == math.floor(n**0.5)):
+                return True
+            return False
+        while(n%4 == 0):
+            n = n//4
+        if(n%8 == 7):
+            return 4
+        if(isSquare(n)):
+            return 1
+        i = 1
+        while(i*i<=n):
+            y = n - i*i
+            if(isSquare(y)):
+                return 2
+            i += 1
+        return 3
