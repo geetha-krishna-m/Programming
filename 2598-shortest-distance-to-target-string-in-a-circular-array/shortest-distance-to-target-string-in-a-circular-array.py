f1a83aa4b52b41e2396070e0fbcea25e56acab05
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-09 22:13:43 -0500

    Time: 57 ms (69.16%) | Memory: 16.9 MB (57.14%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
@@ -4,9 +4,7 @@ class Solution:
             return -1
         w = len(words)
         for i in range(w-1):
-            if(words[(startIndex-i+w)%w] == target):
-                return i
-            if(words[(startIndex+i)%w] == target):
+            if(words[(startIndex-i+w)%w] == target or words[(startIndex+i)%w] == target):
                 return i
         return -1
             
