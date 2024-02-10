ac87b75e84b157851ebb230462e459d9a672786a
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-09 20:04:20 -0500

    Time: 146 ms (32.64%) | Memory: 24.4 MB (21.41%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,11 +1,32 @@
+# class Solution:
+#     def countSubstrings(self, s: str) -> int:
+#         n = len(s)
+#         cnt = 0
+#         for i in range(n):
+#             for j in range(i,n):
+#                 k = s[i:j+1]
+#                 if(k == k[::-1]):
+#                     cnt += 1
+#         return cnt
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
-        cnt = 0
+        palindrome = [[False] * n for _ in range(n)]
+        ans = 0
+
         for i in range(n):
-            for j in range(i,n):
-                k = s[i:j+1]
-                if(k == k[::-1]):
-                    cnt += 1
-        return cnt
-        
+            palindrome[i][i] = True
+            ans += 1
+
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                palindrome[i][i + 1] = True
+                ans += 1
+
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                if s[i] == s[i + length - 1] and palindrome[i + 1][i + length - 2]:
+                    palindrome[i][i + length - 1] = True
+                    ans += 1
+
+        return ans
