de0df9b16e2627469a26d9249ad086d1c94efe4a
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-09 20:06:06 -0500

    Time: 371 ms (15.25%) | Memory: 16.6 MB (65.40%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,32 +1,33 @@
-# class Solution:
-#     def countSubstrings(self, s: str) -> int:
-#         n = len(s)
-#         cnt = 0
-#         for i in range(n):
-#             for j in range(i,n):
-#                 k = s[i:j+1]
-#                 if(k == k[::-1]):
-#                     cnt += 1
-#         return cnt
 class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
-        palindrome = [[False] * n for _ in range(n)]
-        ans = 0
-
+        cnt = 0
         for i in range(n):
-            palindrome[i][i] = True
-            ans += 1
+            k = ""
+            for j in range(i,n):
+                k += s[j]
+                if(k == k[::-1]):
+                    cnt += 1
+        return cnt
+# class Solution:
+#     def countSubstrings(self, s: str) -> int:
+#         n = len(s)
+#         palindrome = [[False] * n for _ in range(n)]
+#         ans = 0
 
-        for i in range(n - 1):
-            if s[i] == s[i + 1]:
-                palindrome[i][i + 1] = True
-                ans += 1
+#         for i in range(n):
+#             palindrome[i][i] = True
+#             ans += 1
 
-        for length in range(3, n + 1):
-            for i in range(n - length + 1):
-                if s[i] == s[i + length - 1] and palindrome[i + 1][i + length - 2]:
-                    palindrome[i][i + length - 1] = True
-                    ans += 1
+#         for i in range(n - 1):
+#             if s[i] == s[i + 1]:
+#                 palindrome[i][i + 1] = True
+#                 ans += 1
 
-        return ans
+#         for length in range(3, n + 1):
+#             for i in range(n - length + 1):
+#                 if s[i] == s[i + length - 1] and palindrome[i + 1][i + length - 2]:
+#                     palindrome[i][i + length - 1] = True
+#                     ans += 1
+
+#         return ans
