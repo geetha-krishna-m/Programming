143de28849b3ee6f8881162c484413f7a1245a69
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-15 09:09:22 -0500

    Time: 499 ms (91.38%) | Memory: 31.5 MB (97.22%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
@@ -1,26 +1,26 @@
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        # nums.sort()
-        # n = len(nums)
-        # sums = sum(nums)
-        # for i in range(n-1,-1,-1):
-        #     if(nums[i]<sums-nums[i]):
-        #         return sums
-        #     sums = sums-nums[i]
-        # return -1
-        sums = sum(nums)
+        nums.sort()
         n = len(nums)
-        while True:
-            if not nums:
-                return -1
-            x = max(nums)
-            if(sums-x<=x):
-                nums = [i for i in nums if i!=x]
-                sums = sums - x*(n-len(nums))
-                n = len(nums)
-            else:
+        sums = sum(nums)
+        for i in range(n-1,-1,-1):
+            if(nums[i]<sums-nums[i]):
                 return sums
+            sums = sums-nums[i]
         return -1
+        # sums = sum(nums)
+        # n = len(nums)
+        # while True:
+        #     if not nums:
+        #         return -1
+        #     x = max(nums)
+        #     if(sums-x<=x):
+        #         nums = [i for i in nums if i!=x]
+        #         sums = sums - x*(n-len(nums))
+        #         n = len(nums)
+        #     else:
+        #         return sums
+        # return -1
                 
             
 
