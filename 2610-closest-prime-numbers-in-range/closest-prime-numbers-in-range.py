3b56d220e6a65bebd7ad9243d9c515f7ba2f8674
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-02-21 11:42:39 -0500

    Time: 53 ms (83.33%) | Memory: 16.5 MB (99.43%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -1,31 +1,33 @@
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
         def isprime(num):
-            if num == 1:
+            if(num==1):
                 return False
-            else:
-                for i in range(2,int(math.sqrt(num))+1):
-                    if(num%i):
-                        continue
-                    return False
+            for i in range(2,int(math.sqrt(num))+1):
+                if(num%i):
+                    continue
+                return False
             return True
         res = [-1,-1]
         nexts,maxy = -1,99999999
+        if((left!=2 and left%2==0)):
+            left += 1
+        if(left<2 and right>2):
+            return [2,3]
         while(left<right+1):
-            if(left!=2 and left%2==0 or left==1):
-                left += 1
-                continue
-            else:
-                if(isprime(left) and nexts!=-1):
-                    if((left-nexts)<maxy):
-                        res = [nexts,left]
-                        maxy = left-nexts
-                    nexts = left
+            if(isprime(left) and nexts!=-1):
+                if((left-nexts)<maxy):
+                    res = [nexts,left]
+                    maxy = left-nexts
                     if(maxy==2):
                         return res
-                if(isprime(left) and nexts==-1):
-                    nexts = left
+                nexts = left
+            if(isprime(left) and nexts==-1):
+                nexts = left
+            if(left<3):
                 left += 1
+                continue
+            left += 2
         return res
                     
 
