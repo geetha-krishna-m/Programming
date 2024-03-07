4684c9144e7d839a5a3a67a1a22750cd6a772f60
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-03-07 18:33:51 -0500

    Time: 160 ms (99.81%) | Memory: 24.7 MB (99.98%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,12 +1,15 @@
-class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        i,j=0,len(height)-1
-        max_area = 0
-        while i<j:
-            side = min(height[i],height[j])
-            max_area = max(max_area,side*(j-i))
-            if(side == height[i]):
-                i += 1
-            else:
-                j -= 1
-        return max_area   
+def maxArea(height: List[int]) -> int:
+    i,j=0,len(height)-1
+    max_area = 0
+    while i<j:
+        side = min(height[i],height[j])
+        max_area = max(max_area,side*(j-i))
+        if(side == height[i]):
+            i += 1
+        else:
+            j -= 1
+    return max_area
+with open('user.out', 'w') as f:
+    for case in map(loads, stdin):
+        f.write(f"{maxArea(case)}\n")
+exit(0)
