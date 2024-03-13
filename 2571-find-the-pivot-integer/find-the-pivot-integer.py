21cbede96467fa2121c044f951ab5d18dc5f70e4
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-03-12 21:10:39 -0400

    Time: 46 ms (60.68%) | Memory: 16.5 MB (59.17%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,13 +1,11 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        prefixSum = []
-        sums = 0
-        for i in range(n,0,-1):
-            sums += i
-            prefixSum.append(sums)
-        sums = 0
+        sums = (n*(n+1))//2
+        cumi = 0
         for i in range(1,n+1):
-            sums += i
-            if(sums == prefixSum[n-i]):
+            cumi += i
+            if(cumi == sums-cumi+i):
                 return i
-        return -1    
+        return -1
+            
+          
