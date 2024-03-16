a0394c2e50c670d6d50b3a2229133d671d2f9ae5
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-03-15 22:16:16 -0400

    Time: 571 ms (99.02%) | Memory: 22.2 MB (70.11%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,14 +1,27 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        mp = {}
-        sum_val = 0
-        max_len = 0
-        for i, num in enumerate(nums):
-            sum_val += 1 if num == 1 else -1
-            if sum_val == 0:
-                max_len = i + 1
-            elif sum_val in mp:
-                max_len = max(max_len, i - mp[sum_val])
+        counter = 0
+        ans = 0
+        hashmap = {}
+        
+        for i in range(len(nums)):
+            
+            if nums[i] == 1:
+                counter += 1
             else:
-                mp[sum_val] = i
-        return max_len
+                counter -= 1
+                
+            if counter == 0:
+                size = i + 1
+                ans = size
+                
+            elif counter in hashmap:
+                size = i - hashmap[counter]
+                if size > ans:
+                    ans = size
+                
+            else:
+                hashmap[counter] = i
+            
+        return ans
+        
