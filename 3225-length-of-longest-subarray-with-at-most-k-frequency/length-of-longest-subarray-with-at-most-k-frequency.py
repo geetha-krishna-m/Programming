06ed1290bcf426ebd1e5c2f56e83df02491f9de5
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-03-29 08:22:48 -0400

    Time: 992 ms (100.00%) | Memory: 31 MB (95.89%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -1,23 +1,40 @@
+# class Solution:
+#     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+#         start,end,n = 0,0,len(nums)
+#         d = dict()
+        
+#         while(end<n):
+#             d[nums[end]] = d.get(nums[end],0)+1 
+#             if(d[nums[end]]>k):
+#                 d[nums[end]] -= 1
+#                 break
+#             end += 1
+#         maxy = end - start
+#         print(maxy)
+#         while(end<n):
+#             d[nums[end]] = d.get(nums[end],0) + 1
+#             if(d[nums[end]] > k):
+#                 while(d[nums[end]]>k and start<end):
+#                     d[nums[start]] -= 1
+#                     start += 1
+#                 maxy = max(maxy,end-start+1)
+#             maxy = max(maxy,end-start+1)
+#             end = end + 1
+#         return maxy      
+from collections import defaultdict
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
-        start,end,n = 0,0,len(nums)
-        d = dict()
+        m_len=0
+        start_ind=0
+        freq=defaultdict(int)
+        for i in range(len(nums)):
+            if freq[nums[i]]==k:
+                #
+                m_len=max(m_len,i-start_ind)
+                while(freq[nums[i]]==k):
+                    freq[nums[start_ind]]-=1
+                    start_ind+=1
+            freq[nums[i]]+=1
+        m_len=max(m_len,i-start_ind+1)
+        return m_len
         
-        while(end<n):
-            d[nums[end]] = d.get(nums[end],0)+1 
-            if(d[nums[end]]>k):
-                d[nums[end]] -= 1
-                break
-            end += 1
-        maxy = end - start
-        print(maxy)
-        while(end<n):
-            d[nums[end]] = d.get(nums[end],0) + 1
-            if(d[nums[end]] > k):
-                while(d[nums[end]]>k and start<end):
-                    d[nums[start]] -= 1
-                    start += 1
-                maxy = max(maxy,end-start+1)
-            maxy = max(maxy,end-start+1)
-            end = end + 1
-        return maxy      
