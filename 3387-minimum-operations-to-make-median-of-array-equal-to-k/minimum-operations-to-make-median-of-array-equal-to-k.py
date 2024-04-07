5781f6fa7b881994bddd7586e47bc633f397a50d
Geetha Krishna Mannava <64373858+Krishna-mannava@users.noreply.github.com>
2024-04-07 12:06:56 -0400

    Time: 633 ms (100.00%) | Memory: 37.5 MB (100.00%) - LeetSync

---
+++ 3387-minimum-operations-to-make-median-of-array-equal-to-k/minimum-operations-to-make-median-of-array-equal-to-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minOperationsToMakeMedianK(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        if(len(nums)<2):
+            return abs(max(nums)-k)
+        mid = len(nums)//2
+        if(nums[mid] == k):
+            return 0
+        elif(nums[mid]<k):
+            cost = 0
+            while(mid<len(nums) and nums[mid]<k):
+                cost += abs(nums[mid]-k)
+                mid += 1
+        else:
+            cost = 0
+            while(mid>-1 and nums[mid]>k):
+                cost += abs(nums[mid]-k)
+                mid -= 1
+        return cost
+            
+        
