50ce3f4a51c7cd3fb33defe6866884f979fac066
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-06-01 22:20:17 -0400

    Time: 35 ms (95.10%) | Memory: 16.5 MB (30.94%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -13,6 +13,10 @@ class Solution:
         """
         prefix array has 0 at the very first index in this solution so we are using j+1 to check in prefix array so no worries
         checking with (j+1) because here we need xor from (j to k) i.e., y and (i to j-1) is already precomputed so we use prefix at i compare to the prefix at j+2
+
+
+        In short at prefix[i] we have required computed so far xor of a and 
+        to find b we remove a from i to k which gives j to k. but instead of using another loop for j we came to know that number of pairs (j,k) w.r.to i can be included in result if the prefix at k is equal to prefix at i if so all the elements b/w i to k are responsible for the result too.
         """
         for i in range(n):
             for j in range(i + 1, n):
