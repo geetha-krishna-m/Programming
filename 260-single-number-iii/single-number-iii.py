0bd994fe3da1112b944aec1e6ea72f09fd51f55a
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-06-01 20:56:48 -0400

    Time: 59 ms (50.07%) | Memory: 18.2 MB (92.18%) - LeetSync

--- 260-single-number-iii/single-number-iii.py
+++ 260-single-number-iii/single-number-iii.py
@@ -4,8 +4,9 @@ class Solution:
         for i in nums:
             xor ^= i
         a,b = 0,0
+        """(xor & -xor) gives you the right most set bit in the result and we can use this to divide the two unique numbers in the nums as their result of xor will never be 0 as they are different and there must be atleast one set bit between them -- super idea"""
         for i in nums:
-            if i & (xor&-xor):
+            if i & (xor&-xor): 
                 a ^= i
                 continue
             b ^= i
