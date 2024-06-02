861948e9bf7ac6166e80742764693cf1d8abe2ff
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-06-01 22:11:28 -0400

    Time: 47 ms (62.85%) | Memory: 16.5 MB (30.94%) - LeetSync

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,15 +1,22 @@
 class Solution:
-    def countTriplets(self, arr: List[int]) -> int:
+    def countTriplets(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
         n = len(arr)
-        prefix = [0] * (n + 1)
-        
-        for i in range(n):
-            prefix[i + 1] = prefix[i] ^ arr[i]
-        
         count = 0
+        prefixXOR = [0] * (n + 1)
+        
         for i in range(n):
-            for k in range(i + 1, n):
-                if prefix[i] == prefix[k + 1]:
-                    count += (k - i)
+            prefixXOR[i+1] = prefixXOR[i] ^ arr[i]
+        """
+        prefix array has 0 at the very first index in this solution so we are using j+1 to check in prefix array so no worries
+        checking with (j+1) because here we need xor from (j to k) i.e., y and (i to j-1) is already precomputed so we use prefix at i compare to the prefix at j+2
+        """
+        for i in range(n):
+            for j in range(i + 1, n):
+                if (prefixXOR[i] == prefixXOR[j+1]):
+                    count += j - i
         
         return count
