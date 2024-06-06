cec635a1806a3e277ec31b9f0d09d8e5fdae2f4f
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-06-05 22:33:41 -0400

    Time: 153 ms (88.84%) | Memory: 18.4 MB (50.76%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.py
@@ -0,0 +1,22 @@
+import collections
+import heapq
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand)%groupSize != 0:
+            return False
+        c = collections.Counter(hand)
+        s = sorted(c)
+        cnt, prev = c[s[0]],0
+        for i in s:
+            # if c[i]>groupSize:
+            #     return False
+            if c[i]>0:
+                for k in range(1,groupSize):
+                    if(i+k not in c):
+                        return False
+                    if (c[i+k]<c[i]):
+                        return False
+                    c[i+k] -= c[i]
+        return True
+            
+                
