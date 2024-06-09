0a30f0fbc48e0235d90d8a5ea4ca6b4ed6c355c7
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-06-09 17:18:22 -0400

    Time: 136 ms (70.53%) | Memory: 17.1 MB (97.89%) - LeetSync

--- 2817-minimum-cost-to-make-all-characters-equal/minimum-cost-to-make-all-characters-equal.py
+++ 2817-minimum-cost-to-make-all-characters-equal/minimum-cost-to-make-all-characters-equal.py
@@ -1,8 +1,8 @@
 class Solution:
     def minimumCost(self, s: str) -> int:
         cost,n = 0,len(s)
-        for i in range(1,len(s)):
-            if(s[i-1] == s[i]):
+        for i in range(len(s)-1):
+            if(s[i] == s[i+1]):
                 continue
-            cost += min(i,n-i)
+            cost += min(i+1,n-i-1)
         return cost
