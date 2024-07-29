c62a8d5040ffc304a9f9f5bb3101683cf091f720
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-07-28 23:59:36 -0400

    Time: 264 ms (85.64%) | Memory: 27.5 MB (26.65%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -2,9 +2,23 @@ class Solution:
     def minDifference(self, nums: List[int]) -> int:
         if(len(nums)<=4):
             return 0
-        nums.sort()
-        one = nums[-4] - nums[0]  # fourth largest - minimum (3 largest changed to minimum)
-        two = nums[-3] - nums[1]
-        three = nums[-2] - nums[2]
-        four = nums[-1] - nums[3]
-        return min(min(one,two),min(three,four))
+        w,x,y,z,a,b,c,d = -999999,-999999,-999999,-999999,999999,999999,999999,999999
+        for i in range(len(nums)):
+            if w<nums[i]:
+                z,y,x,w = y,x,w,nums[i]
+            elif x<nums[i]:
+                z,y,x = y,x,nums[i]
+            elif y<nums[i]:
+                z,y = y,nums[i]
+            elif z<nums[i]:
+                z = nums[i]
+            
+            if(a>nums[i]):
+                d,c,b,a = c,b,a,nums[i]
+            elif(b>nums[i]):
+                d,c,b = c,b,nums[i]
+            elif(c>nums[i]):
+                d,c = c,nums[i]
+            elif(d>nums[i]):
+                d = nums[i]
+        return min(w-d,x-c,y-b,z-a)
