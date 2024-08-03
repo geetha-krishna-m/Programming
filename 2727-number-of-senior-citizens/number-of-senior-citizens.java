9af3ed06f0954d926f24ced831a833e7ea401b66
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-08-03 09:08:30 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (70.50%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -3,9 +3,9 @@ class Solution {
         int ageAboveSixty = 0;
         for(String s:details)
         {
-            char characters[] = s.toCharArray();
-            int tens = (characters[11] - '0');
-            int units = (characters[12]-'0');
+            
+            int tens = (s.charAt(11)- '0');
+            int units = (s.charAt(12)-'0');
             if( (tens*10+units)>60 ){
                         ageAboveSixty += 1;
             }  
