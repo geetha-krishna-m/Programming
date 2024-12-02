6502814652dd0d7026a1e008615685215145a1c0
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-12-02 02:40:23 -0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Bottom - Up DP
+        # Bottom - Up DP (can also be implemented using 3 variables)
         # if(n<=2):
         #     return n
         # else:
@@ -9,13 +9,27 @@ class Solution:
         #     for i in range(3,n+1):
         #         dp[i] = dp[i-1]+dp[i-2]
         # return dp[n]
-        memo = dict()
-        def recursiveDP(n):
-            if(n<=2):
-                return n
-            elif n in memo:
-                return memo[n]
-            else:
-                memo[n] = recursiveDP(n-1)+recursiveDP(n-2)
-                return memo[n]
-        return recursiveDP(n)
+
+        #Top - Down DP
+        # memo = dict()
+        # def recursiveDP(n):
+        #     if(n<=2):
+        #         return n
+        #     elif n in memo:
+        #         return memo[n]
+        #     else:
+        #         memo[n] = recursiveDP(n-1)+recursiveDP(n-2)
+        #         return memo[n]
+        # return recursiveDP(n)
+
+        # Bottom - Up DP (can also be implemented using 3 variables)
+        if(n<=2):
+            return n
+        else:
+            one,two = 1,2
+            step = n
+            for i in range(3,n+1):
+                step = one + two
+                one = two 
+                two = step
+        return step
