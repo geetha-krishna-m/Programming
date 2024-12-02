296ac3def8a4e8491535bb3bea5a32d5ff46bacf
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-12-02 02:38:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (5.22%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,11 +1,21 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if(n<=2):
-            return n
-        else:
-            dp = [0 for i in range(n+1)]
-            dp[1],dp[2] = 1,2
-            for i in range(3,n+1):
-                dp[i] = dp[i-1]+dp[i-2]
-        return dp[n]
-
+        # Bottom - Up DP
+        # if(n<=2):
+        #     return n
+        # else:
+        #     dp = [0 for i in range(n+1)]
+        #     dp[1],dp[2] = 1,2
+        #     for i in range(3,n+1):
+        #         dp[i] = dp[i-1]+dp[i-2]
+        # return dp[n]
+        memo = dict()
+        def recursiveDP(n):
+            if(n<=2):
+                return n
+            elif n in memo:
+                return memo[n]
+            else:
+                memo[n] = recursiveDP(n-1)+recursiveDP(n-2)
+                return memo[n]
+        return recursiveDP(n)
