70b04fd400ad9dbe855316275d8bc251a6506ae0
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-12-08 01:43:22 -0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (10.63%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,12 +1,13 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         pascalTriangle = []
-        for i in range(numRows):
-            intermediateRow = [0 for _ in range(i+1)]
-            for j in range(i+1):
-                if(j==0 or j==i):
+        for i in range(1,numRows+1):
+            intermediateRow = [0 for _ in range(i)]
+            for j in range(i):
+                if(j==0 or j==i-1):
                     intermediateRow[j] = 1
                 else:
-                    intermediateRow[j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j]
+                    intermediateRow[j] = pascalTriangle[i-2][j-1] + pascalTriangle[i-2][j]
             pascalTriangle.append(intermediateRow)
+            print(pascalTriangle)
         return pascalTriangle
