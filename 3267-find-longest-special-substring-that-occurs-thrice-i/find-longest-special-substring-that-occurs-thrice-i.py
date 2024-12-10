e2af7fe8a30e64bb5594dc7099a49478d1c2fa4b
Geetha Krishna Mannava <mgkkratos@gmail.com>
2024-12-10 00:03:09 -0500

    Time: 19 ms (54.32%) | Memory: 17.2 MB (13.91%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,30 +1,22 @@
+from collections import Counter
 class Solution:
     def maximumLength(self, s: str) -> int:
+        counter = defaultdict(int)
         n = len(s)
-        l, r = 1, n
-
-        if not self.helper(s, n, l):
-            return -1
-
-        while l + 1 < r:
-            mid = (l + r) // 2
-            if self.helper(s, n, mid):
-                l = mid
-            else:
-                r = mid
-
-        return l
-
-    def helper(self, s: str, n: int, x: int) -> bool:
-        cnt = [0] * 26
-        p = 0
-
         for i in range(n):
-            while s[p] != s[i]:
-                p += 1
-            if i - p + 1 >= x:
-                cnt[ord(s[i]) - ord('a')] += 1
-            if cnt[ord(s[i]) - ord('a')] > 2:
-                return True
+            ind,temp = i,""
+            while(ind<n and s[ind] == s[i]):
+                temp = temp + s[ind]
+                counter[temp] = counter.get(temp,0) + 1
+                ind = ind + 1
+        max_len = 0
 
-        return False
+        for j, n in counter.items():
+            if n >= 3:
+                if len(j) > max_len:
+                    max_len = len(j)
+
+        if max_len == 0:
+            return -1
+            
+        return max_len
