23e4893e33467e448c543b6097c3c042cfdfe8e2
Geetha Krishna Mannava <mgkkratos@gmail.com>
2025-01-04 01:28:31 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.55%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,31 +5,18 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        temp = dummy
-        # def recurse(l1,l2):
-        #     if(not l1 and not l2):
-        #         return
-        #     if(l1 and l2):
-        #         recurse(l1.next,l2.next)
-        #     elif(l1):
-        #         recurse(l1.next,l2)
-        #     elif(l2):
-        #         recurse(l1,l2.next)
-        sums,c = 0,0
-        while(l1 or l2):
+        temp = ListNode(0)
+        result = temp
+        c = 0
+        while(l1 or l2 or c!=0):
+            sums = c
             if(l1):
                 sums += l1.val
                 l1 = l1.next
             if(l2):
                 sums += l2.val
                 l2 = l2.next
-            temp.val = temp.val + sums
-            c = temp.val//10
-            if(not l1 and not l2 and c==0):
-                continue
-            temp.next = ListNode(c)
-            temp.val = temp.val%10
+            temp.next = ListNode(sums%10)
+            c = sums//10  
             temp = temp.next
-            sums = 0
-        return dummy       
+        return result.next
