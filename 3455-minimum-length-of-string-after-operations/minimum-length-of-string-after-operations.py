d8e85d0a63a0e710c59c41f10cd0726ab8d8b6e1
Geetha Krishna Mannava <mgkkratos@gmail.com>
2025-01-12 20:53:20 -0500

    Time: 249 ms (21.59%) | Memory: 18.6 MB (64.76%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        c = [0]*26
+        n,cnt = len(s),0
+        for i in range(n):
+            c[ord(s[i])-97] += 1
+        for i in range(26):
+            if(c[i]>2 and c[i]%2):
+                cnt += c[i]-1
+            if(c[i]>2 and not c[i]%2):
+                cnt += c[i]-2
+        return n-cnt
+
+        
