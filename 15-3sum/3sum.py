23646c5b00d2f53db5f4ad19cb7f684084b3c1be
Geetha Krishna Mannava <mgkkratos@gmail.com>
2025-01-26 01:04:14 -0500

    Time: 603 ms (44.57%) | Memory: 21 MB (18.08%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,54 +1,25 @@
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         # Sort nums
-#         nums.sort()
-        
-#         # Edge case 1
-#         if len(nums) < 3:
-#             return []
-        
-#         # Initialize
-#         res = []
-        
-#         # Iterate
-#         for i, num in enumerate(nums):
-#             # Sliding window
-#             l = i + 1
-#             r = len(nums) - 1
-            
-#             # Eliminate calculating for same value
-#             if i > 0 and num == nums[i-1]:
-#                 continue
-
-#             while l < r:
-#                 curr_sum = num + nums[l] + nums[r]
-#                 if curr_sum == 0:
-#                     res.append([num, nums[l], nums[r]])
-#                     l += 1
-#                     r -= 1
-                    
-#                     # Eliminate calculating for same value
-#                     while l < r and nums[l]==nums[l-1]:
-#                         l += 1
-                        
-#                 elif curr_sum > 0:
-#                     r -= 1
-#                 else:
-#                     l += 1
-#         return res
-from collections import Counter
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        counter = Counter(nums)
-        nums, triplets = list(counter.keys()), set()
-        if counter[0] >= 3:
-            triplets.add((0, 0, 0))
-        positives, negatives = [n for n in nums if n > 0], [n for n in nums if n < 0]
-        for a in negatives:
-            for b in positives:
-                c = -(a + b)
-                if c in counter and ((c != a and c != b) or counter[c] > 1):
-                    triplets.add(tuple(sorted([a, b, c])))
-        return triplets
-        
+        res = [] 
+        n,i = len(nums),0
+        nums.sort()
+        while(i<n):
+            if(i>0 and nums[i] == nums[i-1]):
+                i += 1
+                continue
+            j = i + 1
+            k = n - 1
+            while(j<k):
+                sums = nums[i]+nums[j]+nums[k]
+                if(sums == 0):
+                    res.append([nums[i],nums[j],nums[k]])
+                    j += 1
+                    k -= 1
+                    while(j<n and nums[j]==nums[j-1]):
+                        j += 1
+                elif(sums > 0):
+                    k -= 1
+                else:
+                    j += 1
+            i = i + 1
+        return res
