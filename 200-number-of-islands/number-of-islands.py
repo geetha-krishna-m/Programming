a93e9f065f315357b6ccb6d206170a755693482d
Geetha Krishna Mannava <mgkkratos@gmail.com>
2025-01-28 10:22:51 -0500

    Time: 250 ms (55.45%) | Memory: 20.3 MB (52.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,15 +1,16 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n = len(grid),len(grid[0])
-        visited = set()
+        # visited = set()
         def checkBoundaries(i,j):
             if(i>=m or j>=n or i<0 or j<0):
                 return True
             return False
         def traverseIsland(i,j):
-            if(checkBoundaries(i,j) or (i,j) in visited or grid[i][j] == "0"):
+            if(checkBoundaries(i,j) or grid[i][j] == "0"):
                 return
-            visited.add((i,j))
+            # visited.add((i,j))
+            grid[i][j] = "0"
             traverseIsland(i+1,j)
             traverseIsland(i,j+1)
             traverseIsland(i-1,j)
@@ -17,7 +18,7 @@ class Solution:
         islandCount = 0
         for i in range(m):
             for j in range(n):
-                if(grid[i][j] == "1" and (i,j) not in visited):
+                if(grid[i][j] == "1"):
                     islandCount += 1
                     traverseIsland(i,j)
         return islandCount
